58b6823ce128d60843e3b64f1e802e22eec1a895
sourabh raje <sour2157@gmail.com>
2015-09-20 19:58:16 +0530

    s to th program
    this program replaces the letter 's' from a given string to 'th'

---
+++ thring.rb
@@ -0,0 +1,15 @@
+
+def thring
+print "Thtring, pleathe!: "
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+  user_input.gsub!(/s/, "th")
+else
+  puts "Nothing to do here!"
+end
+  
+puts "Your string is: #{user_input}"
+end 
+thring
